fe2f39646f53d2b1215a7d5c706691cb3db6561a
Jorge G <jorgegomezweb@gmail.com>
2021-08-19 19:54:48 +0200

    FIX: adding table instead list

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 const taskText = document.querySelector('#task');
 const btn = document.querySelector('#btn');
 const taskContainer = document.querySelector('.task__container');
+const tableBody = document.querySelector('#table__tr');
 
 let id = 1;
 
 const createTask = (e) => {
 	e.preventDefault();
 
-	// const $ul = document.createElement('ul');
-	// const $li = document.createElement('li');
+	const fragment = document.createDocumentFragment();
+
+	const $tdId = document.createElement('td');
+	$tdId.textContent = `${id++}`;
+
+	const $tdTask = document.createElement('td');
+	$tdTask.textContent = `${taskText.value}`;
 
 	if (taskText.value == '') {
 		alert('error');
