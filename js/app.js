8a5ff6a01eb9ff13284b5bd39432f292d570bc0c
Jorge G <jorgegomezweb@gmail.com>
2021-08-19 19:46:01 +0200

    FEAT: Adding basic function

--- js/app.js
+++ js/app.js
@@ -2,20 +2,24 @@ const taskText = document.querySelector('#task');
 const btn = document.querySelector('#btn');
 const taskContainer = document.querySelector('.task__container');
 
+let id = 1;
+
 const createTask = (e) => {
 	e.preventDefault();
 
-	const $ul = document.createElement('ul');
-	const $li = document.createElement('li');
+	// const $ul = document.createElement('ul');
+	// const $li = document.createElement('li');
 
 	if (taskText.value == '') {
 		alert('error');
 	}
 
+	/*
 	$li.textContent = taskText.value;
 	$ul.appendChild($li);
 
 	taskContainer.appendChild($ul);
+	*/
 
 	taskText.value = '';
 };
